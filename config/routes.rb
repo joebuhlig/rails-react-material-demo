bb4dcb3f380b6f53667fcfe4a37063b20972b8c2
Joe Buhlig <joe@joebuhlig.com>
2020-11-14 13:41:09 -0600

    DEV: Add Login and Tasks Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'login#index'
+
+  get 'tasks/index'
+  get 'login/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
