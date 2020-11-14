467d21605d5a15748de1e6e33ecd378b831e1eca
Joe Buhlig <joe@joebuhlig.com>
2020-11-14 15:24:16 -0600

    FEATURE: Add styles to root structure

--- app/javascript/components/login/Index.js
+++ app/javascript/components/login/Index.js
@@ -1,20 +1,46 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Grid from '@material-ui/core/Grid'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import { withStyles } from '@material-ui/core/styles';
+import BrandIcon from 'images/jb-icon.png';
+
+const styles = {
+  brandIcon: {
+    padding: '30px 0px'
+  },
+  shapes: {
+    background: 'blue',
+    height: '100vh',
+    width: '100%'
+  }
+};
 
 class Index extends React.Component {
   render () {
+    const { classes } = this.props;
     return (
       <Grid container>
         <Grid container item xs={5}>
-          <div>Hello kids</div>
+          <Container maxWidth="sm">
+            <Grid item xs={12} className={classes.brandIcon}>
+              <img src={BrandIcon} alt="Birdseye Logo" height="90"/>
+            </Grid>
+            <Grid item xs={12}>
+              <h1>Welcome to Birdseye!</h1>
+            </Grid>
+        </Container>
         </Grid>
         <Grid container item xs={7}>
-          <div>hello world</div>
+          <div className={classes.shapes}></div>
         </Grid>
       </Grid>
     );
   }
 }
 
-export default Index
+Index.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Index)
